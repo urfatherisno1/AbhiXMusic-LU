e27fe2e3c5a4cf92690b69c8360bd46d22d36728
Abhi Singh <156217394+urfatherisno1@users.noreply.github.com>
2026-01-30 06:05:26 +0530

    Handle FloodWait exception and improve logging

--- AbhiXMusic/core/bot.py
+++ AbhiXMusic/core/bot.py
@@ -1,10 +1,10 @@
+import asyncio
 from pyrogram import Client, errors
 from pyrogram.enums import ChatMemberStatus, ParseMode
+from pyrogram.errors import FloodWait 
 
 import config
-
 from ..logging import LOGGER
-
 
 class Hotty(Client):
     def __init__(self):
@@ -28,23 +28,39 @@ class Hotty(Client):
         try:
             await self.send_message(
                 chat_id=config.LOGGER_ID,
-                text=f"<u><b>» {self.mention} ʙᴏᴛ sᴛᴀʀᴛᴇᴅ :</b><u>\n\nɪᴅ : <code>{self.id}</code>\nɴᴀᴍᴇ : {self.name}\nᴜsᴇʀɴᴀᴍᴇ : @{self.username}",
+                text=f"<u><b>» {self.mention} ʙᴏᴛ sᴛᴀʀᴛᴇᴅ :</b></u>\n\nɪᴅ : <code>{self.id}</code>\nɴᴀᴍᴇ : {self.name}\nᴜsᴇʀɴᴀᴍᴇ : @{self.username}",
             )
+        # 🔥 FLOODWAIT FIX START 🔥
+        except FloodWait as e:
+            LOGGER(__name__).warning(f"⚠️ FloodWait Detected! Sleeping for {e.value} seconds...")
+            await asyncio.sleep(e.value) # Wait karega
+            try:
+                # Sleep ke baad dobara try karega
+                await self.send_message(
+                    chat_id=config.LOGGER_ID,
+                    text=f"<u><b>» {self.mention} ʙᴏᴛ sᴛᴀʀᴛᴇᴅ :</b></u>\n\nɪᴅ : <code>{self.id}</code>\nɴᴀᴍᴇ : {self.name}\nᴜsᴇʀɴᴀᴍᴇ : @{self.username}",
+                )
+            except Exception:
+                pass 
+       
+        
         except (errors.ChannelInvalid, errors.PeerIdInvalid):
             LOGGER(__name__).error(
                 "Bot has failed to access the log group/channel. Make sure that you have added your bot to your log group/channel."
             )
-
         except Exception as ex:
             LOGGER(__name__).error(
                 f"Bot has failed to access the log group/channel.\n  Reason : {type(ex).__name__}."
             )
 
-        a = await self.get_chat_member(config.LOGGER_ID, self.id)
-        if a.status != ChatMemberStatus.ADMINISTRATOR:
-            LOGGER(__name__).error(
-                "Please promote your bot as an admin in your log group/channel."
-            )
+        try:
+            a = await self.get_chat_member(config.LOGGER_ID, self.id)
+            if a.status != ChatMemberStatus.ADMINISTRATOR:
+                LOGGER(__name__).error(
+                    "Please promote your bot as an admin in your log group/channel."
+                )
+        except Exception:
+            pass 
 
         LOGGER(__name__).info(f"Music Bot Started as {self.name}")
 
